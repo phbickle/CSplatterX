ae9596f82de1e1890f92292d45751a1520f99061
phbickle <phbickle@gmail.com>
2014-12-05 23:04:58 -0500

    Added Game manager script and TODO list

---
+++ C_SPLATTER_X/Assets/SCRIPTS/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+    
+    //This manager controls:
+        //Colour changes 
+        //Amount of Enemies
+        //Music
+        //Menu
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
